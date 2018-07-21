59a669854283c813279788a07bfe67243353b52f
SujinLee <sjnu21@gmail.com>
2018-07-21 15:25:24 +0900

    btn-menu-act 클래스 토글 기능 추가

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,13 @@
 // Javascript 실습 파일
 
-$(document).keyup(function(e){
-  if(e.keyCode === 27){
-    $('.grid').toggleClass('grid-show');
-  }
+// $(document).keyup(function(e){
+//   if(e.keyCode === 27){
+//     $('.grid').toggleClass('grid-show');
+//   }
+// });
+
+var btn = $('.btn-menu');
+
+btn.click(function(){
+  $(this).toggleClass('btn-menu-act');
 });
